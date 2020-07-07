2089770b5be56e978a8e0b7986a4ae695b9c8feb
Arjun <arjun2000raj@gmail.com>
2020-07-08 00:08:32 +0530

    adding some missing files

---
+++ controllers/authController.js
@@ -0,0 +1,142 @@
+const {promisify} = require('util');
+const jwt = require('jsonwebtoken');
+const User = require('./../models/userModel');
+const catchAsync = require('./../utilities/catchAsync');
+const AppError = require('./../utilities/appError');
+const sendEmail = require('./../utilities/email');
+
+const signToken = id => {
+    return jwt.sign({id}, process.env.JWT_SECRET, 
+        {expiresIn: process.env.JWT_EXPIRES_IN}
+    );
+}
+
+exports.signup = catchAsync (async(req, res, next) =>{
+    const newUser = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        passwordConfirm: req.body.passwordConfirm
+    });
+
+    const token = signToken(newUser._id);
+
+    res.status(201).json({
+        status: 'success',
+        token,
+        data: {
+            user: newUser
+        }
+    });
+});
+
+exports.login = catchAsync( async (req, res, next) => {
+    const {email, password} = req.body;
+
+    // 1.) Check If email and password exist
+
+    if(!email || !password){
+        return next(new AppError('Please provide email and password!', 400));
+    }
+    // 2.) Check If User exists && password is correct
+    const user = await User.findOne({email}).select('+password');
+
+    if(!user || !await user.correctPassword(password, user.password)){
+        return next(new AppError('Incorrect email or password', 401));
+    }
+    // 3.) If ecerything ok, send token to client
+    var token = signToken(user._id);
+    res.status(200).json({
+        status: 'success',
+        token
+    });
+});
+
+exports.protect =catchAsync(async (req, res, next) =>{
+    // 1.) Getting token and check of It's there
+    let token;
+    if(req.headers.authorization && 
+        req.headers.authorization.startsWith('Bearer')
+    ) {
+         token = req.headers.authorization.split(' ')[1];
+
+    }
+
+    if(!token) {
+        return next(
+            new AppError('You are not loged in! Please log in to get access.', 401)
+        );
+    }
+
+    // 2.) Verification token 
+      const decoded = await promisify( jwt.verify)(token, process.env.JWT_SECRET);
+
+    // 3.) Check If user still exists
+    const currentUser = await User.findById(decoded.id);
+    if(!currentUser){
+        return next( new AppError('The user belong to this token does not exist', 401));
+    }
+
+    // 4.) Check If user changed password after the token was issued
+    if(currentUser.changedPasswordAfter(decoded.iat)){
+        return next(
+            new AppError('User recently changed password! Please login again.', 401)
+        );
+    }
+
+    //GRANT ACCESS PROTECTED ROUTE
+    req.user = currentUser;
+    next();
+})
+
+exports.restrictTo = (...roles) => {
+    return (req, res, next) => {
+        if(!roles.includes(req.user.role)){
+            return next(new AppError('You do not have permission to perform this action', 403))
+        }
+
+        next();
+    };
+};
+
+exports.forgotPassword = catchAsync( async(req, res, next) =>{
+    // 1.) Get user based on POSTed emal
+    const user = await User.findOne({email: req.body.email});
+    if(!user){
+        return next(new AppError('There is no user with this email', 404));
+    }
+
+    // 2.) Generate the random reset token
+    const resetToken = user.createPasswordResetToken();
+    await user.save({validateBeforeSave: false});
+
+    // 3.) Send it to user's email
+    const resetURL = `${req.protocol}://${req.get(
+        'host'
+    )}/api/v1/users/resetPassword/${resetToken}`
+
+    const message = `Forgot your passowd? Submit a PATCH request with your new password and
+    passwordCnfirm to: ${resetURL}.\n If your didn't forget your password, Please ignore this email!`;
+    try{
+        await sendEmail({
+
+        email: user.email,
+        subject: 'your password reset token(valid for 10 min)',
+        message
+
+        });
+        res.status(200).json({
+            status: 'success',
+            message: 'Token sent to email!'
+        });
+    }catch(err){
+        console.log(err)
+        user.passwordResetToken = undefined;
+        user.passwordResetExpires = undefined;
+        await user.save({validateBeforeSave: false});
+        return next(new AppError('Ther was an error sending the mail, Try again...', 500));
+    }
+    
+});
+
+exports.resetPassword = (req, res, next) =>{}
---
+++ controllers/tourController.js
@@ -0,0 +1,194 @@
+// const fs = require('fs');
+const Tour = require('./../models/tourModel');
+const APIFeatures = require('./../utilities/apiFeaures');
+const catchAsync = require('./../utilities/catchAsync');
+const AppError = require('../utilities/appError');
+
+exports.aliasTopTours = (req, res, next) =>{
+    req.query.limit = '5';
+    req.query.sort = '-ratingsAverage,price';
+    req.query.fields = 'name,price,ratingsAverage,difficulty';
+    next();
+}
+
+
+exports.getAllTours = catchAsync(async (req, res, next) => {
+        
+    //EXECUTE QUERY
+    const features = new APIFeatures(Tour.find(), req.query)
+        .filter()
+        .sort()
+        .limitFields()
+        .paginate();
+    const tours = await features.query;
+    
+    // SEND RESPOSES
+    res.status(200).json({
+        status: 'success',
+        results: tours.length,
+        data: {
+            tours
+        }
+    });
+    
+});
+
+exports.createTour = catchAsync(async (req, res, next) =>{
+   
+    // const newId = tours[tours.length - 1].userId +1;
+    // const newTour = Object.assign({userId: newId}, req.body);
+    // tours.push(newTour);
+
+    // fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, 
+    //                 JSON.stringify(tours), err =>{
+    //    res.status(201).json({
+            // status: 'success',
+            // data:{
+            //     tour: tours
+            // }
+        // });
+
+    // });
+
+    const newTour = await Tour.create(req.body);
+    res.status(201).json({
+        status: 'success',
+        data: {
+            tour: newTour
+        }
+    });
+});
+
+exports.getTour = catchAsync(async (req, res, next) => {
+    
+    const tour = await Tour.findById(req.params.id)
+    // Tour.findOne({_id: req.params.id})
+    if(!tour){
+        return next(new AppError('No tour Found with that id', 404));
+    }
+
+    res.status(200).json({
+        status: "success",
+        data: {
+            tour
+        } 
+    });
+
+});
+
+exports.updateTour = catchAsync(async (req, res, next) =>{
+    
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        // runValidators = true
+    });
+
+    if(!tour){
+        return next(new AppError('No tour Found with that id', 404));
+    }
+
+    res.status(200).json({
+    status: 'success',
+    data: {
+        tour
+    }
+    });
+});
+
+exports.deleteTour = catchAsync(async (req, res, next) =>{
+    
+   const tour =  await Tour.findByIdAndDelete(req.params.id)
+
+    if(!tour){
+        return next(new AppError('No tour Found with that id', 404));
+    }
+
+    res.status(204).json({
+        status: 'success',
+        data: null
+    });
+});
+
+exports.getTourStats = catchAsync(async (req, res, next) =>{
+   
+    const stats = await Tour.aggregate([
+        {
+            $match: {ratingsAverage: {$gte: 4.5}}
+        },
+        {
+            $group: {
+                _id: {$toUpper: '$difficulty'},
+                numTours: {$sum: 1},
+                numRatings: {$sum: '$ratingsQuantity'},
+                avgRating: {$avg: '$ratingsAverage'},
+                avgPrice: {$avg: '$price'},
+                minPrice: {$min: '$price'},
+                maxPrice: {$max: '$price'}
+            }
+        },
+        {
+            $sort: {
+                avgprice: 1
+            }
+        },
+        // {
+        //     $match: {_id: {$ne:'EASY'}}
+        // }
+        
+    ]);
+
+    
+    res.status(200).json({
+        status: 'success',
+        data: {
+            stats
+        }
+        });
+});
+
+exports.getMonthlyPlan = catchAsync(async (req, res, next) =>{
+    
+    const year = req.params.year * 1;
+    const plan = await Tour.aggregate([
+        {
+            $unwind: '$startDates'
+        },
+        {
+            $match: {
+                startDates: {
+                    $gte: new Date(`${year}-01.01`),
+                    $lte: new Date(`${year}-12-31`)
+                }
+            }
+        },
+        {
+            $group: {
+                _id: {$month: '$startDates'},
+                numTourStarts: {$sum: 1},
+                tours: {$push: '$name'}
+            }
+        },
+        {
+            $addFields: { month: '$_id'}
+        },
+        {
+            $project: {
+                _id: 0
+            }
+        },
+        {
+            $sort: { numTourStarts: -1 }
+        },
+        {
+            $limit: 6
+        }
+    ]);
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            plan
+        }
+        });
+    
+});
