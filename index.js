26e4056c77349586d255a192584364e2d0b04d0e
Arjun <arjun2000raj@gmail.com>
2020-07-10 18:09:31 +0530

    set up headers using HELMET

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const AppError = require('./utilities/appError');
 const tourRouter = require('./routes/tourRoutes');
@@ -10,22 +11,28 @@ const globleErrorHandler = require('./controllers/errorController');
 const app = express();
 
 //1) GLOBAL MIDDLEWARES
+//set HTPP headers
+app.use(helmet());
 
+//Development logging
 if(process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'));
 }
-
+//Limitng request from same API
 const limiter = rateLimit({
     max: 100,
     windowMs: 60 * 60 * 1000,
     message: 'Too many request form this IP, Please try after an hour!'
 });
-
 app.use('/api', limiter);
 
-app.use(express.json());
+//Body Parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+//Serving static file
 app.use(express.static(`${__dirname}/public`));
 
+// Test middleware
 app.use((req, res, next) => {
     // console.log(req.headers);
     req.requestTime = new Date().toISOString();
